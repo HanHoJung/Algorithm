9ae05b07eef21399c95f7e04a0760074c7021283
HanHoJung <gksghwjd0214@naver.com>
2018-12-10 00:48:31 +0900

    ADD 1707.cpp
    이분그래프 DFS, BFS 소스코드

---
+++ Algorithm/1707.cpp
@@ -0,0 +1,112 @@
+#include <queue>
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <stdio.h>
+using namespace std;
+bool visit[20001];
+bool color[20001];
+vector<int> edge[20001];
+
+void dfs(int node, bool setColor) {
+	visit[node] = true;
+	color[node] = setColor;
+
+	for (int i = 0; i < edge[node].size(); i++) {
+
+		int next = edge[node][i];
+		if (visit[next] == false) {
+			color[next] = !setColor;
+			dfs(next, !setColor);
+		}
+
+	}
+}
+
+
+
+void bfs(int start, bool setColor) {
+	queue<int> q;
+	visit[start] = true;
+	color[start] = setColor;
+	q.push(start);
+
+	while (!q.empty()) {
+		int node = q.front();
+		setColor = !color[node];/*!setColor;*/
+		q.pop();
+
+
+		for (int i = 0; i < edge[node].size(); i++) {
+			int next = edge[node][i];
+
+			if (visit[next] == false) {
+
+				visit[next] = true;
+				color[next] = setColor;
+				q.push(next);
+			}
+
+		}
+
+	}
+
+}
+
+
+int main(void) {
+	int k;//testÀÇ °³¼ö
+	scanf("%d", &k);
+
+	while (k--) {
+
+		int v, e;
+		scanf("%d %d", &v, &e);
+
+		memset(edge, 0, sizeof(edge));
+		memset(visit, false, sizeof(visit));
+		memset(color, false, sizeof(color));
+
+		for (int i = 0; i < e; i++) {
+			int start, end;
+			scanf("%d %d", &start, &end);
+
+			edge[start].push_back(end);
+			edge[end].push_back(start);
+		}
+
+
+		for (int i = 1;i <= v; i++) {
+
+			if (visit[i] == false)
+				bfs(i, true);
+				/*dfs(i, true);*/
+				
+			
+
+
+		}
+
+		bool output = true;
+
+		for (int i = 1; i <= v; i++) {
+			for (int j = 0; j < edge[i].size(); j++) {
+				if (color[i] == color[edge[i][j]]) {
+					output = false;
+					break;
+				}
+
+			}
+		}
+
+		if (output == true) {
+			cout << "YES" << "\n";
+		}
+		else {
+			cout << "NO" << "\n";
+
+		}
+
+	}
+
+}
