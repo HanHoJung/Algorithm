b97b4bc8a5078d0037716810f5755050593b16b9
HanHoJung <gksghwjd0214@naver.com>
2018-12-10 00:48:07 +0900

    ADD 11726.cpp
    2*n타일링 소스코드 추가

---
+++ Algorithm/11726.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001];
+int main(void) {
+	int n;
+	cin >> n;
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2])%10007;
+	}
+	cout << dp[n] << "\n";
+}
